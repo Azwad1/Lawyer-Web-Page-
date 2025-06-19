fcb62670eaaf51da29884892f416da8d4e83d2ea
Azwad1 <155709985+Azwad1@users.noreply.github.com>
2025-06-19 18:13:28 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const targetId = link.getAttribute('href');
+    const target = document.querySelector(targetId);
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - 30, 
+        behavior: 'smooth'
+      });
+    }
+  });
+});
